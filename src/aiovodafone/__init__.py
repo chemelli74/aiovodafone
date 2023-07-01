f9a6a59b02a58fe5689edb6b121e3cb579799b13
J. Nick Koston <nick@koston.org>
2023-07-01 16:40:16 -0500

    fix: add some basic sanity tests

--- src/aiovodafone/__init__.py
+++ src/aiovodafone/__init__.py
@@ -1,1 +1,12 @@
 __version__ = "0.0.2"
+
+from .api import VodafoneStationDevice, VodafoneStationApi
+from .exceptions import VodafoneError, CannotConnect, CannotAuthenticate
+
+__all__ = [
+    "VodafoneStationDevice",
+    "VodafoneStationApi",
+    "VodafoneError",
+    "CannotConnect",
+    "CannotAuthenticate",
+]
