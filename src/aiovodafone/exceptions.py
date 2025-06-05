bb282c5720c6730905891ba9966225966c58a85e
Simone Chemelli <simone.chemelli@gmail.com>
2025-06-05 17:56:46 +0000

    chore: cleanup

--- src/aiovodafone/exceptions.py
+++ src/aiovodafone/exceptions.py
@@ -31,10 +31,6 @@ class ModelNotSupported(VodafoneError):
     """Exception raised when using a model not yet supported."""
 
 
-class RequestFailed(VodafoneError):
-    """Exception raised when a debug request failed."""
-
-
 class ResultTimeoutError(VodafoneError):
     """Exception raised when a debug request times out."""
 
